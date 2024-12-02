c595788981baadeba30a20845e1914ebb56e94b0
bsa11 <bruno.santos@sky.uk>
2024-12-02 12:07:55 +0000

    Day 02

--- main.go
+++ main.go
@@ -1,8 +1,13 @@
 package main
 
-import "adventofcode2024/day01"
+import (
+	"adventofcode2024/day01"
+	"adventofcode2024/day02"
+)
 
 func main() {
 	day01.Solve1("input.txt")
 	day01.Solve2("input.txt")
+	day02.Solve1("input.txt")
+	day02.Solve2("input.txt")
 }
