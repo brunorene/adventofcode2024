58565847d35b67a9c500c096d20a4d346f0ac352
bsa11 <bruno.santos@sky.uk>
2024-12-08 09:25:42 +0000

    Day 07

--- main.go
+++ main.go
@@ -1,25 +1,22 @@
 package main
 
 import (
-	"adventofcode2024/day01"
-	"adventofcode2024/day02"
-	"adventofcode2024/day03"
-	"adventofcode2024/day04"
-	"adventofcode2024/day05"
-	"adventofcode2024/day06"
+	"adventofcode2024/day07"
 )
 
 func main() {
-	day01.Solve1("input.txt")
-	day01.Solve2("input.txt")
-	day02.Solve1("input.txt")
-	day02.Solve2("input.txt")
-	day03.Solve1("input.txt")
-	day03.Solve2("input.txt")
-	day04.Solve1("input.txt")
-	day04.Solve2("input.txt")
-	day05.Solve1("input.txt")
-	day05.Solve2("input.txt")
-	day06.Solve1("input.txt")
-	day06.Solve2("input.txt")
+	//day01.Solve1("input.txt")
+	//day01.Solve2("input.txt")
+	//day02.Solve1("input.txt")
+	//day02.Solve2("input.txt")
+	//day03.Solve1("input.txt")
+	//day03.Solve2("input.txt")
+	//day04.Solve1("input.txt")
+	//day04.Solve2("input.txt")
+	//day05.Solve1("input.txt")
+	//day05.Solve2("input.txt")
+	//day06.Solve1("input.txt")
+	//day06.Solve2("input.txt")
+	day07.Solve1("input.txt")
+	day07.Solve2("input.txt")
 }
